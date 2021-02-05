c3917be336521b5727bb0a36c096611f57a0601e
Antoniel Lima <antonielliimma@gmail.com>
2021-02-05 07:46:51 -0300

    removido build folder

--- build/commands/base.command.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseCommand = void 0;
-var BaseCommand = /** @class */ (function () {
-    function BaseCommand() {
-    }
-    return BaseCommand;
-}());
-exports.BaseCommand = BaseCommand;
